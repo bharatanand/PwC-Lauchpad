3fa86d64d1d910850e86fab2add6b676b5649f92
bharatanand <bharatanand@outlook.in>
2023-06-02 21:36:39 +0530

    Code

---
+++ Introduction to Python/News Report Generation.py
@@ -0,0 +1,14 @@
+# Objective:
+# To work with Operators
+
+# Scenario:
+# Japan was hit by a huge Tsunami. Lives and properties were lost. 
+# Many were injured too. A news reporter has arrived to the spot to analyse and generate report  on the number of people alive , 
+# dead and injured. His report also has a statement 
+# seeking public to help the people in need. Can you help him to generate the report by writing a Python program ? 
+
+# Guidelines:  
+# If the dead count or injured count or safe count entered by the user is a negative number, then display the message 
+# "Invalid input" and stop the program.
+# The statement for seeking help should be "Please help the people who are suffering!!!".  
+# Please refer the sample input and output for more clarifications.
