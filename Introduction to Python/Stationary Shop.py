0c8101c979b8d174a7335cdbb1120959da6a72bd
bharatanand <bharatanand@outlook.in>
2023-06-04 02:06:32 +0530

    updations

--- Introduction to Python/Stationary Shop.py
+++ Introduction to Python/Stationary Shop.py
@@ -1,18 +1,18 @@
 # Objective:
 # To work with Operators
 
-# Scenario: 
-# A new stationary shop has been opened in the city. 
-# The owner asks his accountant to take the list of items sold in the store. 
+# Scenario:
+# A new stationary shop has been opened in the city.
+# The owner asks his accountant to take the list of items sold in the store.
 # The list should contain the details of the items and their costs. Help the accountant to
-# generate the prince list by writing a Python program.  Generate list with just 4 products -  
-# A4sheets, pen, pencil and eraser and get the price of the items from the user.  
+# generate the prince list by writing a Python program.  Generate list with just 4 products -
+# A4sheets, pen, pencil and eraser and get the price of the items from the user.
 # Please refer the sample input and output statements for more clarifications.
 
 # Guidelines:
-#     The amount must be displayed with 2 decimal places. 
-#     On entering the product price to be a negative number, 
-#     the program should display the message " Invalid input" and stop the program. 
+#     The amount must be displayed with 2 decimal places.
+#     On entering the product price to be a negative number,
+#     the program should display the message " Invalid input" and stop the program.
 item1 = "A4sheet"
 item2 = "pen"
 item3 = "pencil"
